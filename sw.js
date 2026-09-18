a5a9f8c633c9282e50a95a6f044f222088fd90bb
nigiri0458 <77334247+nigiri0458@users.noreply.github.com>
2026-09-18 18:39:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'holetracer-v1';
+const CACHE = 'holetracer-v2';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
